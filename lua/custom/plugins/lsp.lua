7873752a882ce9639e3e7058ff76998da0ebc55e
catsucci <150689455+catsucci@users.noreply.github.com>
2024-03-15 21:14:00 +0000

    chore: rename <C-T> to <C-t> for consistency

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -55,7 +55,7 @@ return { -- LSP Configuration & Plugins
 
         -- Jump to the definition of the word under your cursor.
         --  This is where a variable was first declared, or where a function is defined, etc.
-        --  To jump back, press <C-T>.
+        --  To jump back, press <C-t>.
         map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
 
         -- Find references for the word under your cursor.
