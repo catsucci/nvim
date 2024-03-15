55d47f77407375f964cdb2bdd0295538c17f60c6
catsucci <150689455+catsucci@users.noreply.github.com>
2024-03-15 21:25:23 +0000

    Move friendly snippets to dependencies of LuaSnip
    Co-authored-by: TJ DeVries <devries.timothyj@gmail.com>

--- lua/custom/plugins/autocompletion.lua
+++ lua/custom/plugins/autocompletion.lua
@@ -14,6 +14,17 @@ return { -- Autocompletion
         end
         return 'make install_jsregexp'
       end)(),
+      dependencies = {
+        -- `friendly-snippets` contains a variety of premade snippets.
+        --    See the README about individual language/framework/plugin snippets:
+        --    https://github.com/rafamadriz/friendly-snippets
+        {
+          'rafamadriz/friendly-snippets',
+          config = function()
+            require('luasnip.loaders.from_vscode').lazy_load()
+          end,
+        },
+      },
     },
     'saadparwaiz1/cmp_luasnip',
 
@@ -22,12 +33,6 @@ return { -- Autocompletion
     --  into multiple repos for maintenance purposes.
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/cmp-path',
-
-    -- If you want to add a bunch of pre-configured snippets,
-    --    you can use this plugin to help you. It even has snippets
-    --    for various frameworks/libraries/etc. but you will have to
-    --    set up the ones that are useful for you.
-    'rafamadriz/friendly-snippets',
   },
   config = function()
     -- See `:help cmp`
