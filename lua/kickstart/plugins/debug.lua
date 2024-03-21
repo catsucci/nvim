b4afa1b0e4aed80766e9822f9fd90241147484a4
catsucci <150689455+catsucci@users.noreply.github.com>
2024-03-21 09:43:32 +0000

    fix: Add nvim-nio as dependency for nvim-dap-ui
    It's a dependency now.

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -18,6 +18,9 @@ return {
     'williamboman/mason.nvim',
     'jay-babu/mason-nvim-dap.nvim',
 
+    -- Required dependency for nvim-dap-ui
+    'nvim-neotest/nvim-nio',
+
     -- Add your own debuggers here
     'leoluz/nvim-dap-go',
   },
