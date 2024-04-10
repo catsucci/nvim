48661d12ba3c370992854e920c87dde79c659021
catsucci <150689455+catsucci@users.noreply.github.com>
2024-04-10 13:47:07 +0000

    feat: Add Neorg plugin for documentation

---
+++ lua/custom/plugins/neorg.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    'vhyrro/luarocks.nvim',
+    priority = 1000,
+    config = true,
+  },
+  {
+    'nvim-neorg/neorg',
+    build = ':Neorg sync-parsers',
+    opts = {
+      load = {
+        ['core.defaults'] = {}, -- Loads default behaviour
+        ['core.concealer'] = {}, -- Adds pretty icons to your documents
+        ['core.dirman'] = { -- Manages Neorg workspaces
+          config = {
+            workspaces = {
+              notes = '~/notes',
+            },
+            default_workspace = 'notes',
+          },
+        },
+      },
+    },
+  },
+}
