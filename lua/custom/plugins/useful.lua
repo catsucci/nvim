fc434dc82cc774fbebf893d913f5691ee9144825
catsucci <150689455+catsucci@users.noreply.github.com>
2024-04-18 12:28:38 +0100

    Add a autotag that uses treesitter

--- lua/custom/plugins/useful.lua
+++ lua/custom/plugins/useful.lua
@@ -1,7 +1,8 @@
 return {
-  { 'tpope/vim-sleuth', }, -- Detect tabstop and shiftwidth automatically
-  { 'mbbill/undotree', }, -- Undotree to not lose work
-  { 'nvim-pack/nvim-spectre', }, -- `:Spectre` to find and replace across project
-  { 'alvan/vim-closetag', },    -- Auto closing tag for html tags
-  { 'andweeb/presence.nvim', }, -- Discord rich presence for neovim
+  { 'tpope/vim-sleuth' }, -- Detect tabstop and shiftwidth automatically
+  { 'mbbill/undotree' }, -- Undotree to not lose work
+  { 'nvim-pack/nvim-spectre' }, -- `:Spectre` to find and replace across project
+  { 'windwp/nvim-ts-autotag' }, -- Use treesitter to auto close and auto rename html tag
+  { 'alvan/vim-closetag' }, -- Auto closing tag for html tags
+  { 'andweeb/presence.nvim' }, -- Discord rich presence for neovim
 }
